714a84dfb0e5925deda69e6a4a6e4468c22f59e9
mohit <mohitgiri1103@gmail.com>
2026-08-11 20:16:30 +0530

    chore: fix import.meta ESM compile issue in commonjs database bundle

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 const prismaClientSingleton = () => {
   const connectionString =
