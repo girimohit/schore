ef84d88db0d1b4023dbfce20c9a1db1fdff7b448
mohit <mohitgiri1103@gmail.com>
2026-08-13 19:50:19 +0530

    feat: persist user theme mode selections using SharedPreferences

--- apps/mobile/lib/core/theme/theme_provider.dart
+++ apps/mobile/lib/core/theme/theme_provider.dart
@@ -4,20 +4,48 @@ import '../../features/bootstrap/bootstrap_notifier.dart';
 import 'color_scheme.dart';
 import 'theme_builder.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 // Provider to manage app-wide ThemeMode
 final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>((ref) {
   return ThemeModeNotifier();
 });
 
 class ThemeModeNotifier extends StateNotifier<ThemeMode> {
-  ThemeModeNotifier() : super(ThemeMode.system);
+  static const _themeKey = 'user_theme_mode';
 
-  void toggleTheme(bool isDark) {
-    state = isDark ? ThemeMode.dark : ThemeMode.light;
+  ThemeModeNotifier() : super(ThemeMode.system) {
+    _loadTheme();
   }
 
-  void setThemeMode(ThemeMode mode) {
+  Future<void> _loadTheme() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final modeString = prefs.getString(_themeKey);
+      if (modeString != null) {
+        state = ThemeMode.values.firstWhere(
+          (e) => e.toString() == modeString,
+          orElse: () => ThemeMode.system,
+        );
+      }
+    } catch (_) {}
+  }
+
+  Future<void> toggleTheme(bool isDark) async {
+    final mode = isDark ? ThemeMode.dark : ThemeMode.light;
     state = mode;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_themeKey, mode.toString());
+    } catch (_) {}
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    state = mode;
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_themeKey, mode.toString());
+    } catch (_) {}
   }
 }
 
